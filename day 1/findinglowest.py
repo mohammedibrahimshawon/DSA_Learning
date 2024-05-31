38ac6ab6c777270007b29c3873df070a0e39708a
mohammed ibrahim shawon <mshawonc3@gmail.com>
2024-05-31 18:30:34 +0600

    dsa day 1

--- day 1/findinglowest.py
+++ day 1/findinglowest.py
@@ -2,9 +2,18 @@ my_array = [7, 12, 9, 4, 11,2]
 maxVal = my_array[0]
 
 for i in my_array:
-    if i > maxVal:
+    if i < maxVal:
         maxVal = i
 
-print('highest value:',maxVal)
+print('lowest value:',maxVal)
 
 #Python
+
+# my_array = [7, 12, 9, 4, 11,2]
+# maxVal = my_array[0]
+
+# for i in my_array:
+#     if i > maxVal:
+#         maxVal = i
+
+# print('highest value:',maxVal)
